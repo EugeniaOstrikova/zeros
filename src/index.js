e76552049e91160b6fe23196acd59c61acaac5a6
EugeniaOstrikova <genechka.ostrikova@gmail.com>
2018-03-02 19:04:02 +0300

    Zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+var resalt = 0;
+for(var i=5; i < number; i = i * 5){
+    debugger;
+    var somResalt = Math.floor(number / i);
+    resalt = resalt + somResalt;
+    somResalt = 0;
+}
+    return resalt;
 }
